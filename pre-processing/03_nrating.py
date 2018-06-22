380bd644c7c2883f187455d51981e46cb5d6785a
Wladston Viana Ferreira Filho <wlad@code.energy>
2018-06-22 17:16:55 -0300

    Discarding non-informative normalized ratings.

--- pre-processing/03_nrating.py
+++ pre-processing/03_nrating.py
@@ -6,6 +6,9 @@
 # v = number of votes for the movie = (votes)
 # m = min votes required for significance (IMDb uses 25,000)
 # C = mean vote across the whole report (currently 7.0)
+#
+# The minimum number of votes for having an informative normalized rating was
+# arbitrarily set at 5,000.
 
 from pymongo import MongoClient
 
@@ -14,18 +17,18 @@ client = MongoClient()
 db = client.imdbws
 
 m = 25000
+M = 5000
 C = 7.0
 
 i= 0
 for mov in db.titles.find({'nrating': {'$exists': False}, 'is_subject': True}):
     i += 1
-    v = mov['numVotes'] or 2
-    if mov['averageRating']:
+    v = mov['numVotes']
+    if v and v >= M:
         R = float(mov['averageRating'].to_decimal())
+        wr = float((R * v) + (C * m))/(v + m)
     else:
-        R = C
-
-    wr = float((R * v) + (C * m))/(v + m)
+        wr = None
 
     db.titles.update({'_id': mov['_id']}, {'$set': {'nrating': wr}})
     if i % 1000 == 0:

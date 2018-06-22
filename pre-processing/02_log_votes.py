57eec681b5bd2d2147abad2ff90ba82acf96caba
Wladston Viana Ferreira Filho <wlad@code.energy>
2018-06-22 16:15:29 -0300

    Minimum number of votes needs continuity.

--- pre-processing/02_log_votes.py
+++ pre-processing/02_log_votes.py
@@ -1,5 +1,5 @@
-# Movies with a null numVotes have received less than 5 votes. We consider they
-# have received 2 votes.
+# Movies with a null numVotes have received between 0 to 4 votes.
+# To keep log_votes continuous, we consider these movies received 4 votes.
 
 from pymongo import MongoClient
 
@@ -11,6 +11,6 @@ i = 0
 for m in db.titles.find({'log_votes': {'$exists': False}, 'is_subject': True}):
         i += 1
         db.titles.update_one({'_id': m['_id']},
-            {'$set': {'log_votes':  math.log(m['numVotes'] or 2)}})
+            {'$set': {'log_votes':  math.log(m['numVotes'] or 4)}})
         if i % 1000 == 0:
             print("{} titles updated.".format(i))

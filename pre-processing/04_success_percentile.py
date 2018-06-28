62f72271b9cd219d154289327f952c90c5d876bc
Wladston Viana Ferreira Filho <wlad@code.energy>
2018-06-28 16:32:08 -0300

    Better way to separate movies into success groups.

--- pre-processing/04_success_percentile.py
+++ pre-processing/04_success_percentile.py
@@ -14,18 +14,22 @@ client = MongoClient()
 db = client.imdbws
 df = pd.DataFrame(list(db.titles.find({'is_subject': True})))
 
-r = df.groupby('startYear').log_votes.apply(lambda x: x.rank(pct=True))
-df['ypct_logvotes'] = r
+g = df.groupby('startYear')
 
-r = df.groupby('startYear').nrating.apply(lambda x: x.rank(pct=True))
-df['ypct_nrating'] = r
+r = g.log_votes.apply(lambda x: x.rank(pct=True))
+df['ypct_votes'] = r
 
-df['pct_logvotes'] = df.log_votes.rank(pct=True)
-df['pct_nrating'] = df.nrating.rank(pct=True)
+r = g.nrating.apply(lambda x: x.rank(pct=True, na_option='keep'))
+df['ypct_rating'] = r
 
-df['pct'] = (df.pct_nrating + df.pct_logvotes).rank(pct=True)
+df['pct_votes'] = df.log_votes.rank(pct=True)
+df['pct_rating'] = df.nrating.rank(pct=True, na_option='keep')
 
-year_rank = lambda x: (x.ypct_logvotes + x.ypct_nrating).rank(pct=True)
+combined_rank = df.pct_rating.fillna(df.pct_votes) + df.pct_votes
+df['pct'] = combined_rank.rank(pct=True)
+
+def year_rank(x):
+   return (x.ypct_rating.fillna(x.ypct_votes) + x.ypct_votes).rank(pct=True)
 g = df.groupby('startYear', as_index=False)
 df['ypct'] = g.apply(year_rank).reset_index(level=0, drop=True)
 
@@ -37,9 +41,9 @@ df.loc[yrank <= 100, 'group'] = 'A'
 i = 0
 for _, r in df.iterrows():
     i += 1
-    data = {'ypct': r['ypct'], 'ypct_votes': r['ypct_logvotes'],
-            'ypct_rating': r['ypct_nrating'], 'pct': r['pct'],
-            'pct_votes': r['pct_logvotes'], 'pct_rating': r['pct_nrating'],
+    data = {'ypct': r['ypct'], 'ypct_votes': r['ypct_votes'],
+            'ypct_rating': r['ypct_rating'], 'pct': r['pct'],
+            'pct_votes': r['pct_votes'], 'pct_rating': r['pct_rating'],
             'group': r['group']}
     db.titles.update({'_id': r['_id']}, {'$set': data})
     if i % 1000 == 0:

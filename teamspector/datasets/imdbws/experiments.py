85f2bf99920c2f57a2d68978265a4145493b4914
Wladston Viana Ferreira Filho <wlad@code.energy>
2019-07-29 15:48:37 -0300

    🎨 Move experiment definitions of out of bin script

---
+++ teamspector/datasets/imdbws/experiments.py
@@ -0,0 +1,8 @@
+experiments = [
+    {'id': 0,
+     'team': ['producer', 'director', 'writer'],
+     'year_start': 1985,
+     'year_end': 2012,
+     'filter': {'is_subject': True}}
+]
+experiments = dict([(x['id'], x) for x in experiments])
